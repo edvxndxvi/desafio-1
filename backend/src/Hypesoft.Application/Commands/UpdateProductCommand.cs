215bbb6aa8d5dee99069c1a094a730dfb2277c91
edvxndxvi <edvandavi1@hotmail.com>
2026-03-06 12:26:20 -0300

    fix(product command): removing required from Id property

--- backend/src/Hypesoft.Application/Commands/UpdateProductCommand.cs
+++ backend/src/Hypesoft.Application/Commands/UpdateProductCommand.cs
@@ -5,7 +5,7 @@ namespace Hypesoft.Application.Commands
 {
 public class UpdateProductCommand : IRequest<Product>
     {
-        public required Guid Id { get; set; }
+        public Guid Id { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
         public decimal Price { get; set; }
